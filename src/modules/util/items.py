31e70c34a8f1fde41328a449bdb9763355962711
tkosht <take@TakehitonoMacBook-Pro.local>
2020-11-02 01:00:42 +0900

    mod: item to be able to readonly

--- src/modules/util/items.py
+++ src/modules/util/items.py
@@ -1,4 +1,7 @@
 class Item(dict):
+    def __init__(self, is_readonly=False):
+        self.is_readonly = is_readonly
+
     def setup(self, d: dict):
         self.update(d)
         _setup(self, d)
@@ -6,25 +9,31 @@ class Item(dict):
     def __str__(self):
         return str(vars(self))
 
+    def __setattr__(self, name, value):
+        if getattr(self, "is_readonly", False):
+            if name in self.__dict__:
+                raise self.ConstError(f"Can't rebind const ({name})")
+        self.__dict__[name] = value
+
 
 class Items(dict):
-    def __init__(self):
-        pass
+    def __init__(self, is_readonly=True):
+        self.is_readonly = is_readonly
 
     def setup(self, d: dict):
-        _setup(self, d)
+        _setup(self, d, self.is_readonly)
 
     def get(self, key: str):
         return _get(self, key)
 
 
 # set the member name as key
-def _setup(obj, d: dict):
+def _setup(obj, d: dict, is_readonly: bool):
     obj.update(d)
     for k, v in d.items():
         if isinstance(v, dict):
-            itm = Item()
-            _setup(itm, v)
+            itm = Item(is_readonly)
+            _setup(itm, v, is_readonly)
             setattr(obj, k, itm)
             continue
         setattr(obj, k, v)
@@ -64,10 +73,13 @@ if __name__ == '__main__':
             optimizer="Adam"
         )
     )
+
+    # test for accesibility
     itm = Items()
     itm.setup(d)
 
-    # test access member variables
+    ## test access member variables
+    ### like dic or member variable
     assert itm["first"]["lr"] == d["first"]["lr"]
     assert itm.first.lr == d["first"]["lr"]
     assert itm.get("first").lr == d["first"]["lr"]
@@ -78,11 +90,21 @@ if __name__ == '__main__':
     assert itm["second"] == itm.get("second")
     assert id(itm.second) == id(itm.get("second"))
 
+    ### using dict parameters
     def _call_function(loss: str, optimizer: str) -> None:
         assert loss == "cross_entropy"
         assert optimizer == "Adam"
 
     cmn = itm.get("common")
     _call_function(**cmn)
+
+    # test for readonly
+    itm = Items(readonly=True)
+    itm.setup(d)
+    try:
+        itm.second = {"second": "abc"}
+        assert False
+    except TypeError as e:
+        assert True
 
     print("OK")
