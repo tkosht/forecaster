186376326c374b3687f651c854c29e7d73d741bf
tkosht <takehito.oshita.business@gmail.com>
2020-11-09 00:31:40 +0900

    mod: refactored

--- src/modules/model/model.py
+++ src/modules/model/model.py
@@ -2,6 +2,9 @@ import inspect
 import numpy
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from typing import Tuple
+from torch.utils.tensorboard import SummaryWriter
 from ..util.items import Items
 
 
@@ -14,19 +17,28 @@ class M(nn.Module):
         _frame = inspect.currentframe()
         _locals = _frame.f_back.f_back.f_locals
         assert _frame.f_back.f_back.f_locals["__class__"] == type(self)
-        params = {k : v for k, v in _locals.items() if k not in ["self"] and k[:1] != "_"}
-        self.params = Items().setup(params)
+        args = {k: v for k, v in _locals.items() if k not in ["self"] and k[:1] != "_"}
+        self.args = Items().setup(args)
 
 
 class Model(M):
     def __init__(self, dim_ins=16, n_heads=4, ws=8, n_quantiles=7):
-        super().__init__()      # after this call, to be enabled to access `self.params`
-        self.cyclic = Cyclic(dim_ins=dim_ins, n_heads=4, n_quantiles=self.params.n_quantiles)
+        super().__init__()  # after this call, to be enabled to access `self.args`
+        self.cyclic = Cyclic(
+            dim_ins=dim_ins, n_heads=4, n_quantiles=self.args.n_quantiles
+        )
         self.trend = None
         self.recent = None
-        self.ws = ws        # window size
+        self.ws = ws  # window size
 
-    def forward(self, ti: torch.Tensor, tc: torch.Tensor = None, kn: torch.Tensor = None, un: torch.Tensor = None):
+    def forward(
+        self,
+        ti: torch.Tensor,
+        tg: torch.Tensor,
+        tc: torch.Tensor = None,
+        kn: torch.Tensor = None,
+        un: torch.Tensor = None,
+    ):
         r"""
         Args:
             ti: time index
@@ -42,7 +54,7 @@ class Model(M):
             W: window size
             D?: dims
         """
-        cyclic = self.cyclic(ti, tc, kn)
+        cyclic = self.cyclic(ti, tc, kn, tg)
         trend = self.trend(ti, tc, kn)
         ws = self.ws
         recent = self.recent(ti, kn, tc[:, -ws:, :], un[:, -ws:, :])
@@ -51,27 +63,44 @@ class Model(M):
 
 
 def create_linears(dim_emb, kq, ws):
-    return nn.Seuquential(nn.Linear(dim_emb*ws, dim_emb), nn.ReLU(), nn.Linear(dim_emb, kq))
+    return nn.Seuquential(
+        nn.Linear(dim_emb * ws, dim_emb), nn.ReLU(), nn.Linear(dim_emb, kq)
+    )
 
 
 class Cyclic(M):
-    def __init__(self, dim_ins: tuple, dim_out: int, ws: int, dim_emb=5, n_heads=4, k=3, n_quantiles=7, n_layers=2):
-        super().__init__()      # after this call, to be enabled to access `self.params`
+    def __init__(
+        self,
+        dim_ins: tuple,
+        dim_out: int,
+        ws: int,
+        dim_emb=5,
+        n_heads=4,
+        k=3,
+        n_quantiles=7,
+        n_layers=2,
+    ):
+        super().__init__()  # after this call, to be enabled to access `self.args`
         self.emx = nn.Linear(sum(dim_ins[1:]), dim_emb)
         self.emy = nn.Linear(1, dim_emb)
 
         # Transformers
-        prm = dict(d_model=dim_emb, nhead=n_heads, num_encoder_layers=n_layers, num_decoder_layers=n_layers)
+        prm = dict(
+            d_model=dim_emb,
+            nhead=n_heads,
+            num_encoder_layers=n_layers,
+            num_decoder_layers=n_layers,
+        )
         self.tra = nn.Transformer(**prm)
         self.trb = nn.Transformer(**prm)
         self.trw = nn.Transformer(**prm)
         self.tro = nn.Transformer(**prm)
 
         # linears
-        self.ak = nn.Linear(dim_emb*ws, k*n_quantiles)
-        self.bk = nn.Linear(dim_emb*ws, k*n_quantiles)
-        self.wk = nn.Linear(dim_emb*ws, k)
-        self.ok = nn.Linear(dim_emb*ws, k)
+        self.ak = nn.Linear(dim_emb * ws, k * n_quantiles)
+        self.bk = nn.Linear(dim_emb * ws, k * n_quantiles)
+        self.wk = nn.Linear(dim_emb * ws, k)
+        self.ok = nn.Linear(dim_emb * ws, k)
 
         # initialize weights
         nn.init.kaiming_normal_(self.ak.weight)
@@ -79,7 +108,9 @@ class Cyclic(M):
         nn.init.xavier_normal_(self.wk.weight)
         nn.init.xavier_normal_(self.ok.weight)
 
-    def forward(self, ti: torch.Tensor, tc: torch.Tensor, kn: torch.Tensor, tg: torch.Tensor):
+    def forward(
+        self, ti: torch.Tensor, tc: torch.Tensor, kn: torch.Tensor, tg: torch.Tensor
+    ):
         # embedding
         x = torch.cat([tc, kn], dim=-1)
         emx = self.emx(x)
@@ -88,7 +119,7 @@ class Cyclic(M):
         # transform
         # h = self.tr(emx, emy)
         def reshape(tsr: torch.Tensor):
-            return tsr.view(-1, self.params.dim_emb * self.params.ws)
+            return tsr.view(-1, self.args.dim_emb * self.args.ws)
 
         ha = reshape(self.tra.encoder(emx))
         hb = reshape(self.trb.encoder(emx))
@@ -100,59 +131,150 @@ class Cyclic(M):
         o = torch.sigmoid(self.ok(ho)) / numpy.pi
 
         # adjusting the shape
-        kq = self.params.k * self.params.n_quantiles
+        kq = self.args.k * self.args.n_quantiles
         t = ti[:, -1, :].repeat(1, kq)
-        w = w.view(-1, self.params.k, 1).repeat(1, 1, 7).view(-1, kq)
-        o = o.view(-1, self.params.k, 1).repeat(1, 1, 7).view(-1, kq)
+        w = w.view(-1, self.args.k, 1).repeat(1, 1, 7).view(-1, kq)
+        o = o.view(-1, self.args.k, 1).repeat(1, 1, 7).view(-1, kq)
 
         # calculate theta (rad)
         th = 2 * numpy.pi * w * t + o
         y = a * torch.cos(th) + b * torch.sin(th)
-        y = y.view(-1, self.params.k, self.params.n_quantiles)
+        y = y.view(-1, self.args.k, self.args.n_quantiles)
         y = y.sum(dim=1)
 
         return y
 
 
-def _create_toydataset(B, W, Dout):
-    """
-    ti = [  [[1], [2], [3], [4]],
-            [[2], [3], [4], [5]],
-            [[3], [4], [5], [6]],
-            [[4], [5], [6], [7]],
-            [[5], [6], [7], [8]],
-            :
-            ]
-    ti.shape = (B, W, 1)
-    """
-    t0 = torch.arange(0, W).view(1, W, Dout)
-    ti = t0
-    for idx in range(B-1):
-        t1 = t0 + idx + 1
-        ti = torch.cat([ti, t1], axis=0)
-    ti = ti.float()
+class Trainer(object):
+    def __init__(self, model, optimizer, criterion, writer, params):
+        self.model = model
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.writer = writer
+        self.params = params
 
-    tc = torch.rand((B, 1, 1)).repeat(1, W, 3)  # shape: (B, W, Dtc)
-    kn = torch.rand((B, W, 4))  # shape: (B, W, Dkn)
-    tg = torch.sin(ti).repeat(1, 1, Dout)  # target
-    # tg = (torch.sin(2*ti) + 3*torch.sin(ti) + 0.5*torch.cos(3*ti)).repeat(1, 1, Dout)
-    return ti, tc, kn, tg
+    def get_quantile(self, x: torch.Tensor, alpha: float):
+        idx = (numpy.array(self.params.quantiles) == alpha).argmax()
+        return x[:, idx, :][..., 0]  # just to get a first dim
+
+    def do_train(self, dataset: Items, epochs: int = 500):
+        ti, tc, kn, tg = dataset.ti, dataset.tc, dataset.kn, dataset.tg
+        # train loop
+        batch_losses = []
+        losses = []
+        for idx in range(epochs):
+            shuffle = numpy.random.permutation(range(len(ti)))
+            t = ti[shuffle]
+
+            def closure():
+                self.optimizer.zero_grad()
+                y_pred = self.model(t, tc, kn, tg)
+                loss = self.criterion(y_pred, tg[:, -1, :], self.params.quantiles)
+                batch_losses.append(loss.item())
+
+                # logging progress
+                if idx % self.params.log_intervals == 0 and idx > 0:
+                    mean_loss = numpy.array(batch_losses[idx - 20 : idx]).mean()
+                    print(f"loss[{idx:03d}]", mean_loss)
+                    losses.append(mean_loss)
+                    self.writer.add_scalar("loss/train", loss, idx)
+
+                    def make_predictions(
+                        y_pred, tg
+                    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+                        pred = y_pred.view(
+                            -1, len(self.params.quantiles), self.model.args.dim_out
+                        )
+                        p = self.get_quantile(pred, alpha=0.5)
+                        p10 = self.get_quantile(pred, alpha=0.1)
+                        p90 = self.get_quantile(pred, alpha=0.9)
+                        t = tg[:, -1, :][..., 0]
+                        return p, p10, p90, t
+
+                    def write_log2tb(preds, pred_type="train") -> None:
+                        for n, (y0, yL, yH, t0) in enumerate(zip(*preds)):
+                            dct_pred = dict(p=y0, p10=yL, p90=yH, t=t0)
+                            self.writer.add_scalars(
+                                f"prediction/epoch_{idx}/{pred_type}", dct_pred, n
+                            )
+
+                    # prediction with trainset
+                    preds = make_predictions(y_pred, tg)
+                    write_log2tb(preds, "train")
+
+                    # prediction with testset
+                    test_ti, test_tc, test_kn, test_tg = toydataset.create_testset()
+                    with torch.no_grad():
+                        _y_pred = self.model(test_ti, test_tc, test_kn, test_tg)
+                    preds = make_predictions(_y_pred, test_tg)
+                    write_log2tb(preds, "valid")
+
+                loss.backward()
+                return loss
+
+            self.optimizer.step(closure)
+
+        self.writer.close()
 
 
-def _make_testset(ti: torch.Tensor, tc: torch.Tensor, kn: torch.Tensor, tg: torch.Tensor):
-    offset = 10
-    sz = 16
-    test_ti = (ti + len(ti))[offset:offset+sz]
-    test_tc = tc[offset:offset+sz]
-    test_kn = kn[offset:offset+sz]
-    test_tg = tg[offset:offset+sz]
-    return test_ti, test_tc, test_kn, test_tg
+class DatasetToy(object):
+    def __init__(self, B: int, W: int, Dout: int):
+        self.B = B
+        self.W = W
+        self.Dout = Dout
+        self.ti = torch.Tensor([])
+        self.tc = torch.Tensor([])
+        self.kn = torch.Tensor([])
+        self.tg = torch.Tensor([])
+
+    def create(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        ti = [  [[1], [2], [3], [4]],
+                [[2], [3], [4], [5]],
+                [[3], [4], [5], [6]],
+                [[4], [5], [6], [7]],
+                [[5], [6], [7], [8]],
+                :
+                ]
+        ti.shape = (B, W, 1)
+        """
+        B, W, Dout = self.B, self.W, self.Dout
+        t0 = torch.arange(0, W).view(1, W, Dout)
+        ti = t0
+        for idx in range(B - 1):
+            t1 = t0 + idx + 1
+            ti = torch.cat([ti, t1], axis=0)
+        self.ti = ti.float()
+        self.tc = torch.rand((B, 1, 1)).repeat(1, W, 3)  # shape: (B, W, Dtc)
+        self.kn = torch.rand((B, W, 4))  # shape: (B, W, Dkn)
+        # self.tg = torch.sin(ti).repeat(1, 1, Dout)  # target
+        self.tg = (
+            torch.sin(2 * self.ti)
+            + 3 * torch.sin(self.ti)
+            + 0.5 * torch.cos(3 * self.ti)
+        ).repeat(1, 1, Dout)
+        return self.ti, self.tc, self.kn, self.tg
+
+    def create_testset(
+        self, offset=10, sz=16
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        ti, tc, kn, tg = self.ti, self.tc, self.kn, self.tg
+        test_ti = (ti + len(ti))[offset : offset + sz]
+        test_tc = tc[offset : offset + sz]
+        test_kn = kn[offset : offset + sz]
+        test_tg = tg[offset : offset + sz]
+        return test_ti, test_tc, test_kn, test_tg
 
 
-def quantile_loss(pred_y, tg, quantiles=[0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95]):
+def quantile_loss(
+    pred_y: torch.Tensor,
+    tg: torch.Tensor,
+    quantiles=[0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95],
+) -> torch.Tensor:
+
     losses = []
     for idx, qtl in enumerate(quantiles):
-        err = tg - pred_y[..., idx].unsqueeze(-1)       # (B, 1)
+        err = tg - pred_y[..., idx].unsqueeze(-1)  # (B, 1)
         losses.append(torch.max((qtl - 1) * err, qtl * err).unsqueeze(-1))  # (B, 1, 1)
     losses = torch.cat(losses, dim=2)
     loss = losses.sum(dim=(1, 2)).mean()
@@ -160,112 +282,46 @@ def quantile_loss(pred_y, tg, quantiles=[0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95])
     return loss
 
 
-def get_quantile(x: torch.Tensor, alpha: float):
-    idx = (numpy.array(cri_params.quantiles) == alpha).argmax()
-    return x[:, idx, :][..., 0]     # just to get a first dim
-
-
-def do_predict(ti: torch.Tensor, tc: torch.Tensor, kn: torch.Tensor, tg: torch.Tensor, name="trainset"):
-    with torch.no_grad():
-        y_pred = model(ti, tc, kn, tg)
-
-        # arrange outputs
-        y_pred = y_pred.view(-1, len(cri_params.quantiles), Dout)
-        p = get_quantile(y_pred, alpha=.5)
-        p10 = get_quantile(y_pred, alpha=.1)
-        p90 = get_quantile(y_pred, alpha=.9)
-        t = tg[:, -1, :][..., 0]
-
-        # plot and save
-        df = pandas.DataFrame({"p": p, "t": t})
-        df.plot(figsize=(10, 5))
-        pyplot.fill_between(df.index, p10, p90, facecolor='b', alpha=0.2)
-        pyplot.savefig(f"img/pred_{name}.png")
-    return
-
-
 if __name__ == "__main__":
     import datetime
-    import torch.optim as optim
-    from torch.utils.tensorboard import SummaryWriter
 
     # create toy dataset
     B, W, Dout = 64, 4, 1
 
-    ti, tc, kn, tg = _create_toydataset(B, W, Dout)
+    toydataset = DatasetToy(B, W, Dout)
+    ti, tc, kn, tg = toydataset.create()
+    dataset = Items(is_readonly=True).setup(dict(ti=ti, tc=tc, kn=kn, tg=tg))
 
     # setup model
     dims = (ti.shape[-1], tc.shape[-1], kn.shape[-1])
-    model = Cyclic(dim_ins=dims, dim_out=tg.shape[-1], ws=ti.shape[1], dim_emb=4*2, n_heads=2, k=3, n_quantiles=7)
+    model = Cyclic(
+        dim_ins=dims,
+        dim_out=tg.shape[-1],
+        ws=ti.shape[1],
+        dim_emb=4 * 2,
+        n_heads=2,
+        k=3,
+        n_quantiles=7,
+    )
 
     # setup optimizer and criterion
     # optimizer = optim.LBFGS(model.parameters(), lr=0.8)     # Newton
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
     # optimizer = optim.SGD(model.parameters(), lr=0.001)
-
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
     criterion = quantile_loss
-    cri_params = Items().setup(dict(quantiles=[0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95]))
 
     # setup tensorboard writer
-    experiment_id = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    experiment_id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     logdir = f"result/{experiment_id}"
-    tbwriter = SummaryWriter(log_dir=logdir)
+    writer = SummaryWriter(log_dir=logdir)
 
-    N = 10 * 1000
-    batch_losses = []
-    losses = []
-    for idx in range(N):
-        shuffle = numpy.random.permutation(range(len(ti)))
-        t = ti[shuffle]
+    params = Items(is_readonly=True).setup(
+        dict(
+            quantiles=[0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95],
+            log_intervals=100,
+        )
+    )
+    trainer = Trainer(model, optimizer, criterion, writer, params)
 
-        def closure():
-            optimizer.zero_grad()
-            y_pred = model(t, tc, kn, tg)
-            loss = criterion(y_pred, tg[:, -1, :], **cri_params)
-            batch_losses.append(loss.item())
-            if idx % 100 == 0 and idx > 0:
-                mean_loss = numpy.array(batch_losses[idx-20: idx]).mean()
-                print(f"loss[{idx:03d}]", mean_loss)
-                losses.append(mean_loss)
-                tbwriter.add_scalar("loss/train", loss, idx)
-
-                def make_predictions(y_pred, tg):
-                    pred = y_pred.view(-1, len(cri_params.quantiles), Dout)
-                    p = get_quantile(pred, alpha=.5)
-                    p10 = get_quantile(pred, alpha=.1)
-                    p90 = get_quantile(pred, alpha=.9)
-                    t = tg[:, -1, :][..., 0]
-                    return p, p10, p90, t
-
-                preds = make_predictions(y_pred, tg)
-                for n, (y0, yL, yH, t0) in enumerate(zip(*preds)):
-                    _dct_pred = dict(p=y0, p10=yL, p90=yH, t=t0)
-                    tbwriter.add_scalars(f"prediction/epoch_{idx}/train", _dct_pred, n)
-
-                test_ti, test_tc, test_kn, test_tg = _make_testset(ti, tc, kn, tg)
-                with torch.no_grad():
-                    _y_pred = model(test_ti, test_tc, test_kn, test_tg)
-                preds = make_predictions(_y_pred, test_tg)
-                for n, (y0, yL, yH, t0) in enumerate(zip(*preds)):
-                    _dct_pred = dict(p=y0, p10=yL, p90=yH, t=t0)
-                    tbwriter.add_scalars(f"prediction/epoch_{idx}/valid", _dct_pred, n)
-
-            loss.backward()
-            return loss
-        optimizer.step(closure)
-
-    import pandas
-    from matplotlib import pyplot
-
-    df = pandas.DataFrame({"loss": losses})
-    df.plot(figsize=(10, 5))
-    pyplot.savefig("img/loss.png")
-
-    # predict for trainset
-    do_predict(ti, tc, kn, tg, "trainset")
-
-    # predict for testset
-    test_ti, test_tc, test_kn, test_tg = _make_testset(ti, tc, kn, tg)
-    do_predict(test_ti, test_tc, test_kn, test_tg, "testset")
-
-    tbwriter.close()
+    # train
+    trainer.do_train(dataset, epochs=10 * 1000)
